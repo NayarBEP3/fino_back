b2813aee10ae28c25383f3430570f9dd3c84191e
Brayan Estrada <brayan.estrada.work@gmail.com>
2023-06-21 08:52:50 -0500

    Feature: User validation added

--- src/main/java/com/tiamat/fino/services/AccountService.java
+++ src/main/java/com/tiamat/fino/services/AccountService.java
@@ -3,7 +3,9 @@ package com.tiamat.fino.services;
 import com.tiamat.fino.dtos.accounts.AccountDto;
 import com.tiamat.fino.dtos.accounts.AddAccountDto;
 import com.tiamat.fino.entities.AccountEntity;
+import com.tiamat.fino.entities.UserEntity;
 import com.tiamat.fino.repositories.AccountRepository;
+import com.tiamat.fino.repositories.UserRepository;
 import com.tiamat.fino.utils.ValidationUtils;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +18,17 @@ import java.util.Optional;
 @Service
 public class AccountService {
     private final AccountRepository repository;
+    private final UserRepository userRepository;
     private final ModelMapper mapper = new ModelMapper();
 
-    public AccountService(@Autowired AccountRepository repository) {
+    public AccountService(@Autowired AccountRepository repository, @Autowired UserRepository userRepository) {
         this.repository = repository;
+        this.userRepository = userRepository;
     }
 
     public AccountDto addAccount(AddAccountDto addAccountDto) {
         addAccountDto.validate();
+        this.validateUserById(addAccountDto.getUserId());
         AccountEntity accountEntity = mapper.map(addAccountDto, AccountEntity.class);
         accountEntity = repository.save(accountEntity);
         return mapper.map(accountEntity, AccountDto.class);
@@ -43,6 +48,7 @@ public class AccountService {
     public AccountDto updateAccount(AddAccountDto addAccountDto, String accountId) {
         addAccountDto.validate(accountId);
         this.getAccount(accountId);
+        this.validateUserById(addAccountDto.getUserId());
         AccountEntity accountEntity = mapper.map(addAccountDto, AccountEntity.class);
         accountEntity.setId(accountId);
         accountEntity = repository.save(accountEntity);
@@ -53,6 +59,11 @@ public class AccountService {
         Optional<AccountEntity> accountDto = repository.findById(accountId);
         ValidationUtils.getValueFromOptional(accountDto, "Error: Account not found.");
         return mapper.map(accountDto.get(), AccountDto.class);
+    }
+
+    private void validateUserById(String userId) {
+        Optional<UserEntity> userEntityOptional= userRepository.findById(userId);
+        ValidationUtils.getValueFromOptional(userEntityOptional, "Error: User not found.");
     }
 
 }
